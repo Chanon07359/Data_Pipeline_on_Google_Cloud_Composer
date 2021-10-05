4bd7dd364472eb65b37222617a9a3913115337e5
Mr.Chanon Sattrupinat <s6001091620058@email.kmutnb.ac.th>
2021-10-06 00:11:22 +0700

    commit

--- Data_Pipeline_in_airflow.py
+++ Data_Pipeline_in_airflow.py
@@ -21,7 +21,7 @@ def get_data_from_DB(raw_data_from_DB_path):
 def clean_data(raw_data_from_DB_path,accident_table_path,list_nafill):
     accident_table=pd.read_csv(raw_data_from_DB_path)
     accident_table.drop(['Title','Sub Industry','Company Name','Number of Punished','Financial Penalty'], axis=1, inplace=True)
-    accident_table["City"] = accident_table.apply(lambda x: x["City"].replace(",","'"), axis=1)
+    accident_table["City"] =accident_table["City"].replace(",","'")
     accident_table['Date']=pd.to_datetime(accident_table['Date'])
     for i in list_nafill :
         accident_table[i].fillna("Unknow", inplace = True)
@@ -52,14 +52,10 @@ with DAG(
         bash_command="bq load --source_format=CSV --autodetect [DATASET].[TABLE_NAME] gs://[GCS_BUCKET]/data/raw_accident_table.csv" #Upload to BQ by bq command
     )
 
-    t4 =GCSToBigQueryOperator(
-        task_id="Move_final_data_to_BQ",
-        bucket='[GCS_BUCKET]',
-        source_objects=['data/accident_table.csv'],
-        destination_project_dataset_table='[DATASET].[TABLE_NAME]',
-        autodetect=True,
-        write_disposition='WRITE_TRUNCATE'
-    )                                                                                                                                 #Upload to BQ by GCSToBigQueryOperator
+    t4 =BashOperator(
+        task_id="Backup_final_data_to_BQ",
+        bash_command="bq load --source_format=CSV --autodetect [DATASET].[TABLE_NAME] gs://[GCS_BUCKET]/data/accident_table.csv" #Upload to BQ by bq command
+    )                                                                                                                                #Upload to BQ by GCSToBigQueryOperator
 
     t1 >> [t2,t3] >> t4 
 
